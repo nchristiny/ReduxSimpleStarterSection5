085eb94f3de5f57df58155adf0921cf22cc2922d
Nick Christiny <nchristiny@jellyvision.com>
2016-03-22 11:41:02 -0500

    Make first AJAX request

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -16,8 +20,9 @@ export default class SearchBar extends Component {
   onFormSubmit(event) {
     event.preventDefault();
     // Here we would need to go and fetch weather data
-    // Call the axios creator!
-
+    this.props.fetchWeather(this.state.term);
+    // clear the search input as a nice thing for the user
+    this.setState({ term: '' });
   }
 
   render() {
@@ -35,3 +40,9 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
