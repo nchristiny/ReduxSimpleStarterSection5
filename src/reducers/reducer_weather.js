d3d27117d668f3ac4048dd697ab69965147d01f1
Nick Christiny <nchristiny@jellyvision.com>
2016-03-22 15:55:17 -0500

    Finish mapping props to render helper

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,6 @@
 import { FETCH_WEATHER } from '../actions/index';
 
-export default function(state = null, action) {
+export default function(state = [], action) {
     switch (action.type) {
       case FETCH_WEATHER:
         // BAD do not manipulate (or state) state directly, [city,
