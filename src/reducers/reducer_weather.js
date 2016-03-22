9ab79f1ecc2e227ac2a37b711e6996987de387d7
Nick Christiny <nchristiny@jellyvision.com>
2016-03-22 15:00:50 -0500

    Finish reducer_weather, Lecture 59

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,16 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
 
+export default function(state = null, action) {
+    switch (action,type) {
+      case FETCH_WEATHER:
+        // BAD do not manipulate (or state) state directly, [city,
+        // [city, [city]]]
+        // return state.push[ action.payload.data ];
+        // BETTER
+        // return state.concat([action.payload.data]);
+        // BEST (ES6) - this does not mutate the state, returns entirely new array,
+        //  [city, city]
+        return [ action.payload.data, ...state];
+      }
   return state;
 }
