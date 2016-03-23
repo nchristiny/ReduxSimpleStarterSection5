d4910d7cb16f6d22392eff4f7ae5a7b9fa791605
Nick Christiny <nchristiny@jellyvision.com>
2016-03-23 11:36:13 -0500

    Add avg, label to charts. Add imperial units to API call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '921738689bc7bdff57608c39f70e2c7b';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=imperial`;
 // ES6 template strings  on line above
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
